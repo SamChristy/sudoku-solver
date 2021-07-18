2637cd7365e137ecf51cead4e45a31aa464a267b
Sam Christy <sam_christy@hotmail.co.uk>
2021-07-18 17:49:10 +0100

    Use dataloader to make tests way smaller

--- src/services/__tests__/SudokuScanner.test.ts
+++ src/services/__tests__/SudokuScanner.test.ts
@@ -1,79 +1,42 @@
 import { loadImage } from 'canvas';
-import md5 from 'md5';
+import { readdirSync } from 'fs';
+import path from 'path';
 
 import { SudokuScanner } from '../index';
 
 const testImageDir = `${__dirname}/samples`;
+const { saveCanvas } = global;
 
-it('loads without crashing', () => {
+it('loads libraries without crashing', () => {
   const canvas = document.createElement('canvas');
   const scanner = new SudokuScanner(canvas);
   scanner.destruct();
 });
 
-it('loads image', async () => {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
-  const image = await loadImage(`${testImageDir}/dog.jpg`);
-  ctx?.drawImage((image as unknown) as ImageBitmap, 0, 0);
-
-  const scanner = new SudokuScanner(canvas);
-  scanner.destruct();
-});
-
-it('scans image not containing sudoku', async () => {
+it('reads image without crashing', async () => {
   const canvas = document.createElement('canvas');
   const ctx = canvas.getContext('2d');
   const image = await loadImage(`${testImageDir}/dog.jpg`);
   ctx?.drawImage((image as unknown) as ImageBitmap, 0, 0);
 
   const scanner = new SudokuScanner(canvas);
-  const found = scanner.extractSudokuImage();
   scanner.destruct();
-
-  expect(found).toBe(false);
 });
 
-it('finds sudoku in photo', async () => {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
-  const image = await loadImage(`${testImageDir}/sudoku-photo.jpg`);
-  ctx?.drawImage((image as unknown) as ImageBitmap, 0, 0);
+describe.each(readdirSync(testImageDir))('finds and extracts sudoku puzzle', filename =>
+  test(filename, async () => {
+    const inputCanvas = document.createElement('canvas');
+    const outputCanvas = document.createElement('canvas');
+    const ctx = inputCanvas.getContext('2d');
+    const image = await loadImage(`${testImageDir}/${filename}`);
 
-  const scanner = new SudokuScanner(canvas);
-  const found = scanner.extractSudokuImage();
-  scanner.destruct();
+    inputCanvas.width = image.width;
+    inputCanvas.height = image.height;
+    ctx?.drawImage((image as unknown) as ImageBitmap, 0, 0);
 
-  expect(found).toBe(true);
-});
-
-it('finds sudoku in graphic', async () => {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
-  const image = await loadImage(`${testImageDir}/sudoku-graphic.png`);
-  ctx?.drawImage((image as unknown) as ImageBitmap, 0, 0);
-
-  const scanner = new SudokuScanner(canvas);
-  const found = scanner.extractSudokuImage();
-  scanner.destruct();
-
-  expect(found).toBe(true);
-});
-
-it('effectively crops and flattens sudoku photo', async () => {
-  const md5Snapshot = '3d6ef67de2ceccaf5b5d0e95697a6f07';
-  const inputCanvas = document.createElement('canvas');
-  const outputCanvas = document.createElement('canvas');
-  const ctx = inputCanvas.getContext('2d');
-  const image = await loadImage(`${testImageDir}/sudoku-photo.jpg`);
-
-  inputCanvas.width = image.width;
-  inputCanvas.height = image.height;
-  ctx?.drawImage((image as unknown) as ImageBitmap, 0, 0);
-
-  const scanner = new SudokuScanner(inputCanvas);
-  scanner.extractSudokuImage(outputCanvas);
-  scanner.destruct();
-
-  expect(md5(outputCanvas.toDataURL())).toBe(md5Snapshot);
-});
+    const scanner = new SudokuScanner(inputCanvas);
+    scanner.extractSudokuImage(outputCanvas);
+    scanner.destruct();
+    saveCanvas(outputCanvas, `${testImageDir}/../output/${path.parse(filename).name}.png`);
+  })
+);
