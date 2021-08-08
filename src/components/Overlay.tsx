9669941d1f3910133c68c920ab0c66d3a087c2a8
Sam Christy <sam_christy@hotmail.co.uk>
2021-08-08 13:40:08 +0100

    ⌜⌝Add reticle corners⌞⌟

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -3,10 +3,10 @@ import styles from './Overlay.module.scss';
 export default function Overlay() {
   return (
     <div className={styles.overlay} role="application">
-      <div className={`${styles.blurBox} ${styles.top}`} />
-      <div className={`${styles.blurBox} ${styles.right}`} />
-      <div className={`${styles.blurBox} ${styles.bottom}`} />
-      <div className={`${styles.blurBox} ${styles.left}`} />
+      <div className={`${styles.corner} ${styles.top} ${styles.left}`} />
+      <div className={`${styles.corner} ${styles.top} ${styles.right}`} />
+      <div className={`${styles.corner} ${styles.bottom} ${styles.left}`} />
+      <div className={`${styles.corner} ${styles.bottom} ${styles.right}`} />
     </div>
   );
 }
