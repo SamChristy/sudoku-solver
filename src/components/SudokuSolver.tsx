0eaa401322196b33229de72890a7443428e7cb4e
Sam Christy <sam_christy@hotmail.co.uk>
2022-08-16 22:40:09 +0100

    Make the Sudoku Solver... actually solve Sudokus! 🥳

--- src/components/SudokuSolver.tsx
+++ src/components/SudokuSolver.tsx
@@ -1,19 +1,39 @@
+import structuredClone from '@ungap/structured-clone';
+
+import { SudokuSolver as SudokuSolverService } from '../services';
 import { Sudoku } from '../types/interfaces/SudokuSolver';
 import styles from './SudokuSolver.module.scss';
 
 export default function SudokuSolver({ sudoku }: { sudoku: Sudoku }) {
-  let i = 0;
+  // If we don't clone it, the original sudoku is modified and a wierd occurs, where all the table
+  // cells are rendered as original!
+  const sudokuSolver = new SudokuSolverService(structuredClone(sudoku));
+  const solved = sudokuSolver.isValid() ? sudokuSolver.solve() : null;
+
+  let key = 0;
   return (
     <table className={styles.sudokuSolver}>
       <tbody>
-        {sudoku.map(row => (
-          <tr key={i}>
-            {row.map(cell => (
-              // eslint-disable-next-line no-plusplus -- 🙄
-              <td key={i++}>{cell || ''}</td>
-            ))}
-          </tr>
-        ))}
+        {solved ? (
+          solved.map((row, r) => (
+            <tr key={key}>
+              {row.map((cell, c) => (
+                // eslint-disable-next-line no-plusplus -- 🙄
+                <td key={key++} className={sudoku[r][c] ? styles.original : ''}>
+                  {cell}
+                </td>
+              ))}
+            </tr>
+          ))
+        ) : (
+          <div className={styles.error}>
+            <div>
+              <h2>Scan Failed</h2>
+              <p>Please try to ensure the image is clear and not obstructed by anything.</p>
+              <p>(PS: webcams or partially-completed sudokus likely won&apos;t work!)</p>
+            </div>
+          </div>
+        )}
       </tbody>
     </table>
   );
